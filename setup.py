20ad8d39f2b6aaca01955ab3438127c4c8c640a6
Eli Gothill <eli.gothill@getbyrd.com>
2021-05-02 21:10:15 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/gothill/pyradix",
-    version="version='0.0.1'",
+    version="0.0.1",
     zip_safe=False,
 )
