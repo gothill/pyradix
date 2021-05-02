89bcce7493f86c665141b92155aa5bed45c8a8af
Eli Gothill <eli.gothill@getbyrd.com>
2021-05-02 21:05:03 +0200

    Version

--- pyradix/__init__.py
+++ pyradix/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for pyradix."""
 
 __author__ = """Eli Gothill"""
-__email__ = 'egothill@gmail.com'
-__version__ = '0.1.1'
+__email__ = "egothill@gmail.com"
+__version__ = "0.0.1"
